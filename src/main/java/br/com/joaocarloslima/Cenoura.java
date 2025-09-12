ac925a7a973365a0e6657a285643693029e288df
lucas.gbsantos5 <lucasgabriel405@gmail.com>
2025-09-12 20:26:42 -0300

    correção cenoura

--- src/main/java/br/com/joaocarloslima/Cenoura.java
+++ src/main/java/br/com/joaocarloslima/Cenoura.java
@@ -1,30 +1,30 @@
 package br.com.joaocarloslima;
 
 public class Cenoura {
-        private int tamanho;
-        private int tempoDeVida;
-        private int tempoDeCrescimento = 4;
+    private int tamanho;
+    private int tempoDeVida;
+    private int tempoDeCrescimento = 4;
 
-        public void crescer(){
-            tempoDeVida += 1;
-            if (tempoDeVida == tempoDeCrescimento){
-                tamanho += 1;
-                tempoDeCrescimento += 4;
-            }
-
-        }
-
-        public boolean podeColher(){
-            if (tamanho == 4){
-                return true;
-            }
-            return false;
-        }
-
-        public String getImagem(){
-            return "imagem/cenoura" + tamanho +".png";
+    public void crescer() {
+        tempoDeVida += 1;
+        if (tempoDeVida == tempoDeCrescimento) {
+            tamanho += 1;
+            tempoDeCrescimento += 4;
         }
 
     }
 
+    public boolean podeColher() {
+        if (tamanho == 4) {
+            return true;
+        }
+        return false;
+    }
+
+    public String getImagem() {
+        return "imagem/cenoura" + tamanho + ".png";
+    }
+
 }
+
+
