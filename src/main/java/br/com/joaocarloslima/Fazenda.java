546cd455c1f5119af136b007f5e70b7309b1c4dd
lucas.gbsantos5 <lucasgabriel405@gmail.com>
2025-09-12 21:30:47 -0300

    Adicao de quase Fazenda

---
+++ src/main/java/br/com/joaocarloslima/Fazenda.java
@@ -0,0 +1,33 @@
+package br.com.joaocarloslima;
+
+import java.util.List;
+
+public class Fazenda {
+    private List<Terreno> terrenos;
+    private Celeiro celeiro;
+
+    public Fazenda() {
+    }
+    public int plantarBatata(int x, int y){
+
+    }
+    public int plantarCenoura(int x, int y){
+
+    }
+    public int plantarMorango(int x, int y){
+
+    }
+
+    public Terreno getTerreno(int x, int y){
+
+    }
+
+    public colher(int x, int y){
+
+    }
+
+    public Celeiro getCeleiro() {
+        return Celeiro;
+    }
+}
+
